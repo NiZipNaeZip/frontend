e32b65b67599d1a1d08c201e224ff84fefb41b3e
100Gyeon <bjy9966@naver.com>
2022-11-02 20:15:21 +0900

    chore: Recoil 관련 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '@src/styles/globalStyle';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </RecoilRoot>
   );
 }
 
