dd0e704cab0611167cf2e1707bf766986d1e2f6e
100Gyeon <bjy9966@naver.com>
2022-11-03 00:47:54 +0900

    chore: 디테일 수정

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,12 @@
+import SEO from '@src/components/common/SEO';
 import PlaceSearchBox from '@src/components/Register/PlaceSearchBox';
 
 function Search() {
   return (
-    <div>
+    <>
+      <SEO title="우편번호 검색" />
       <PlaceSearchBox />
-    </div>
+    </>
   );
 }
 
